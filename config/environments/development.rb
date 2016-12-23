c5c400b9777380535f7a6fdb79f982b6f3fe3a73
Matthew Chinn <mattchinn96@gmail.com>
2016-12-22 19:11:04 -0800

    small changes

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,7 +1,7 @@
 Rails.application.configure do
     #for mailer
     config.action_mailer.delivery_method = :smtp
-    config.action_mailer.perform_deliveries = true
+    config.action_mailer.perform_deliveries = false
     # Don't care if the mailer can't send.
     config.action_mailer.raise_delivery_errors = true
     # SMTP settings for gmail
